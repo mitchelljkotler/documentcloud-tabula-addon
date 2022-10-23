112bb674b6559028d1ea320e4db21d5132b729b7
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-10-23 09:55:45 -0500

    Allows for table extraction from multiple files

--- main.py
+++ main.py
@@ -16,28 +16,29 @@ class Tabula(AddOn):
     """A tabula Add-On for DocumentCloud"""
 
     def main(self):
-        document = self.client.documents.get(self.documents[0])
-        url = self.data['url']
-        if url is not None: 
-            with open("template.json", "wb") as template_file:
-                resp = requests.get(url)
-                template_file.write(resp.content)
-            pdf_name = f"{document.title}.pdf"
+        
+        for document in self.get_documents():
+            url = self.data['url']
+            if url is not None: 
+                with open("template.json", "wb") as template_file:
+                    resp = requests.get(url)
+                    template_file.write(resp.content)
+                pdf_name = f"{document.title}.pdf"
             
-            with open("file.pdf", "wb") as pdf_file:
-                pdf_file.write(document.pdf)
-            data_frame_list = tabula.read_pdf_with_template("./file.pdf", "template.json")
-            for data_frame in data_frame_list:
-                data_frame.to_csv(f"{document.slug}.csv", mode='a', index=False, header=False)
-            with open(f"{document.slug}.csv") as csv_file:
-                self.upload_file(csv_file)
+                with open("file.pdf", "wb") as pdf_file:
+                    pdf_file.write(document.pdf)
+                data_frame_list = tabula.read_pdf_with_template("./file.pdf", "template.json")
+                for data_frame in data_frame_list:
+                    data_frame.to_csv(f"{document.slug}.csv", mode='a', index=False, header=False)
+                with open(f"{document.slug}.csv") as csv_file:
+                    self.upload_file(csv_file)
 
-        else: 
-            with open(f"{document.slug}.pdf", "wb") as pdf_file:
-                pdf_file.write(document.pdf)
-            tabula.convert_into(f"{document.slug}.pdf", f"{document.slug}.csv", output_format="csv", pages="all")
-            with open(f"{document.slug}.csv") as csv_file:
-                self.upload_file(csv_file)
+            else: 
+                with open(f"{document.slug}.pdf", "wb") as pdf_file:
+                    pdf_file.write(document.pdf)
+                tabula.convert_into(f"{document.slug}.pdf", f"{document.slug}.csv", output_format="csv", pages="all")
+                with open(f"{document.slug}.csv") as csv_file:
+                    self.upload_file(csv_file)
 
 if __name__ == "__main__":
     Tabula().main()
