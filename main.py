4f799756ef1dab304cc40e87d89486d0e91222be
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-10-23 20:40:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,53 @@ https://github.com/chezou/tabula-py
 It allows you to export tables from a set of PDFs and allows you to download the results as a zip file. 
 """
 
-import os.path
+import os
 import tabula
 import pandas
 import requests
 import zipfile
+from urllib.parse import urlparse
 from documentcloud.addon import AddOn
-
+import lootdl
+from lootdl import DROPBOX_URL, GDRIVE_URL, MEDIAFIRE_URL, WETRANSFER_URL
 
 class Tabula(AddOn):
     """A tabula Add-On for DocumentCloud"""
 
+    def fetch_files(self, url):
+        """Fetch the files from either a cloud share link or any public URL"""
+        self.set_message("Downloading the files...")
+
+        os.makedirs(os.path.dirname("./out/"), exist_ok=True)
+        cloud_urls = [DROPBOX_URL, GDRIVE_URL, MEDIAFIRE_URL, WETRANSFER_URL]
+        if any(cloud_url in url for cloud_url in cloud_urls):
+            # surpress output during lootdl download to avoid leaking
+            # private information
+            stdout = sys.stdout
+            sys.stdout = open(os.devnull, "w")
+            lootdl.grab(url, "./out/")
+            # restore stdout
+            sys.stdout = stdout
+        else:
+            parsed_url = urlparse(url)
+            basename = os.path.basename(parsed_url.path)
+            title, ext = os.path.splitext(basename)
+            if not title:
+                title = "template"
+            if not ext:
+                ext = "json"
+            with requests.get(url, stream=True) as resp:
+                resp.raise_for_status()
+                with open(f"{title}.{ext}", "wb") as json_file:
+                    for chunk in resp.iter_content(chunk_size=8192):
+                        json_file.write(chunk)
+                 
     def main(self):
         with zipfile.ZipFile("export.zip", mode="w") as archive:
             for document in self.get_documents():
                 url = self.data['url']
                 if url is not None: 
-                    with open("template.json", "wb") as template_file:
-                        resp = requests.get(url)
-                        template_file.write(resp.content)
+                    self.fetch_files(url)
                     pdf_name = f"{document.title}.pdf"
             
                     with open("file.pdf", "wb") as pdf_file:
