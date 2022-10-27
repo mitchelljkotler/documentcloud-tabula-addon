79c1c79c67fd3b41669206b2a33603e38a634e16
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-10-27 17:28:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Tabula(AddOn):
     """A tabula PDF table extraction Add-On for DocumentCloud"""
 
     def fetch_template(self, url):
-        """Fetch the files from either Dropbox, Google Drive, or any public URL"""
+        """Fetch the template from either Dropbox, Google Drive, or any public URL"""
         os.makedirs(os.path.dirname("./out/"), exist_ok=True)
         if(grab(url, "./out/")):
             for file in os.listdir('./out/'):
@@ -44,6 +44,27 @@ class Tabula(AddOn):
                 else:
                     return False
            
+    def template_based_extract(self, url):
+        if(self.fetch_template(url)):
+            with zipfile.ZipFile("export.zip", mode="w") as archive:
+                for document in self.get_documents():
+                    with open("file.pdf", "wb") as pdf_file:
+                        pdf_file.write(document.pdf)
+                    data_frame_list = tabula.read_pdf_with_template("./file.pdf", "./out/template.json") 
+                    # Tabula's read_pdf_with_template() returns a list of data frames we can append to form a CSV. 
+                    for data_frame in data_frame_list:
+                        data_frame.to_csv(f"{document.slug}.csv", mode="a", index=False, header=False)
+                archive.write(f"{document.slug}.csv")
+        else:
+            self.template_less_extract():
+    
+    def template_less_extract(self):
+        with zipfile.ZipFile("export.zip", mode="w") as archive:
+            for document in self.get_documents():
+                pdf_file.write(document.pdf)
+                tabula.convert_into(f"{document.slug}.pdf", f"{document.slug}.csv", output_format="csv", pages="all",) 
+                # Tabula's convert_into() guesses boundaries for table extraction. 
+            archive.write(f"{document.slug}.csv")
 
     def main(self):
         """
@@ -53,25 +74,12 @@ class Tabula(AddOn):
         the boundaries for each file
         """
         url = self.data["url"]
-        self.fetch_template(url)
-        with zipfile.ZipFile("export.zip", mode="w") as archive:
-            for document in self.get_documents():
-                if url is not None: #the if branch gets executed if a template is provided
-                    with open("file.pdf", "wb") as pdf_file:
-                        pdf_file.write(document.pdf)
-                    data_frame_list = tabula.read_pdf_with_template("./file.pdf", "./out/template.json") 
-                    # Tabula's read_pdf_with_template() returns a list of data frames we can append to form a CSV. 
-                    for data_frame in data_frame_list:
-                        data_frame.to_csv(
-                            f"{document.slug}.csv", mode="a", index=False, header=False)
-                else: #the else branch gets executed if no template is provided. 
-                    with open(f"{document.slug}.pdf", "wb") as pdf_file: 
-                        pdf_file.write(document.pdf)
-                    tabula.convert_into(f"{document.slug}.pdf", f"{document.slug}.csv", output_format="csv", pages="all",) 
-                    # Tabula's convert_into() guesses boundaries for table extraction. 
-                archive.write(f"{document.slug}.csv")
+        if url is not None:
+            self.template_based_extract(url)
+        else:
+            self.template_less_extract()
+        
         self.upload_file(open("export.zip"))
-
 
 if __name__ == "__main__":
     Tabula().main()
